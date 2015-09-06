34632f77f0499dd598097291571ff1a8d38924e8
Nagisa Furukawa <dgbrl58@gmail.com>
2015-09-05 19:40:35 -0500

    Update createfolder.sh

--- createfolder.sh
+++ createfolder.sh
@@ -3,4 +3,4 @@
 read -p "Enter into the directory of choice. Slashes please   : " directory
 alias proj="cd $directory" #enters into the directory you want to use the folder inb
 read -p "Enter in your folder name     : " folder 
-mkdir $folder	#creates the folder using the mkdir command, works for ubuntu but I'm not sure about other distros!
+mkdir $folder	#creates an empty folder with mkdir
